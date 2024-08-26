99834cf60b9a6c94018ebe23357d03c5c3e2d541
Pablo Mesén <127916341+pablomesen@users.noreply.github.com>
2024-08-25 21:39:41 -0600

    RESET v2

--- src/app.py
+++ src/app.py
@@ -1,8 +1,6 @@
 # Inicia la RestAPI, la DB, define las rutas y permite su acceso desde cualquier origen.
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .routes import users, posts
-from .db import DBInstance
 
 app = FastAPI(title="TC01", version="0.1.0")
 
@@ -14,15 +12,9 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(users.router, prefix="/users", tags=["Users"])
-app.include_router(posts.router, prefix="/posts", tags=["Posts"])
-
 @app.get("/")
 async def root():
     return {"message": "Bienvenido a la TC01 de BD2! :) Accede a /docs para ver la documentación de endpoints."}
-
-# Crea las tablas definidas en los modelos antes de iniciar la aplicación
-DBInstance.Base.metadata.create_all(bind=DBInstance.engine)
 
 if __name__ == "__main__":
     import uvicorn
